af395b6a954b101ccf6543edde23f3ed9e7d1e4b
SietalAI <SietalAi@outlook.com>
2025-06-18 01:40:58 -0600

    Update state.rs

--- Contracts/state.rs
+++ Contracts/state.rs
@@ -1,1 +1,8 @@
-#PLACEHOLDER
+use solana_program::pubkey::Pubkey;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AgentData {
+    pub owner: Pubkey,
+    pub preferences: Vec<u8>,
+}
