6a9e958f01e87301d529a88f9205747a0a7407f0
SietalAI <SietalAi@outlook.com>
2025-06-18 01:40:22 -0600

    Update lib.rs

--- Contracts/lib.rs
+++ Contracts/lib.rs
@@ -1,1 +1,17 @@
-#PLACEHOLDER
+pub mod state;
+pub mod processor;
+pub mod errors;
+
+use solana_program::{
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    processor::process(program_id, accounts, instruction_data)
+}
