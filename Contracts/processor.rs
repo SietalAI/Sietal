5848acf4cb5c0dff21415779ecd15407c6678bf2
SietalAI <SietalAi@outlook.com>
2025-06-18 01:40:40 -0600

    Update processor.rs

--- Contracts/processor.rs
+++ Contracts/processor.rs
@@ -1,1 +1,9 @@
-#PLACEHOLDER
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
+
+pub fn process(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    Ok(())
+}
